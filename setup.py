4e743d3903601b2f3f52bdd37ba6025b7984f0f7
baminou <brice.aminou@gmail.com>
2018-09-17 11:26:59 -0400

    README.md changed to .rst

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import find_packages
 
 setup(
     name='uperations-kernel',
-    version='0.0.2',
+    version='0.0.4',
     packages=find_packages(),
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    long_description=open("README.md").read(),
+    long_description=open("README.rst").read(),
     author='Brice Aminou',
     author_email='brice.aminou@gmail.com',
     keywords='workflow tool',
